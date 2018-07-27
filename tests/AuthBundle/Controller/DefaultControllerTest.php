bb312c026e49a2eaf6b0af58c82b4b5ce5a3df35
Vinay <vinays@wekancode.com>
2018-07-27 17:36:33 +0530

    Update test case

--- tests/AuthBundle/Controller/DefaultControllerTest.php
+++ tests/AuthBundle/Controller/DefaultControllerTest.php
@@ -6,12 +6,67 @@ use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
 
 class DefaultControllerTest extends WebTestCase
 {
-    public function testIndex()
+    public function testRegistrationWithIncompleteFields()
     {
         $client = static::createClient();
 
-        $crawler = $client->request('GET', '/');
+        $crawler = $client->request('POST', '/auth/register', [
+            'name' => '',
+            'email' => '',
+            'password' => ''
+        ]);
 
-        $this->assertContains('Hello World', $client->getResponse()->getContent());
+        $this->assertEquals($client->getResponse()->getStatusCode(), 422);
+    }
+
+    public function testRegistration()
+    {
+        $client = static::createClient();
+
+        $crawler = $client->request('POST', '/auth/register', [
+            'name' => 'john doe',
+            'email' => 'johndoe@gmail.com',
+            'password' => 'password'
+        ]);
+
+        $this->assertEquals($client->getResponse()->getStatusCode(), 200);
+        //$this->assertArrayHasKey('data', $data);
+    }
+
+    public function testRegistrationWithExistingEmail()
+    {
+        $client = static::createClient();
+
+        $crawler = $client->request('POST', '/auth/register', [
+            'name' => 'john doe',
+            'email' => 'johndoe@gmail.com',
+            'password' => 'password'
+        ]);
+
+        $this->assertEquals($client->getResponse()->getStatusCode(), 422);
+    }
+
+    public function testLoginWithWrongCredentials(){
+        $client = static::createClient();
+
+        $crawler = $client->request('POST', '/auth/login', [
+            'email' => 'johndoe@gmail.com',
+            'password' => 'password123'
+        ]);
+
+        $this->assertEquals($client->getResponse()->getStatusCode(), 401);
+    }
+
+    public function testLoginWithRightCredentials(){
+        $client = static::createClient();
+
+        $crawler = $client->request('POST', '/auth/login', [
+            'email' => 'johndoe@gmail.com',
+            'password' => 'password'
+        ]);
+
+        $this->assertEquals($client->getResponse()->getStatusCode(), 200);
+        $data = json_decode($client->getResponse()->getContent(), true);
+        $this->assertArrayHasKey('token', $data["data"]);
     }
 }
--- tests/UserBundle/Controller/DefaultControllerTest.php
+++ tests/UserBundle/Controller/DefaultControllerTest.php
@@ -3,15 +3,76 @@
 namespace UserBundle\Tests\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
+use AuthBundle\Traits\JwtTrait;
+
 
 class DefaultControllerTest extends WebTestCase
 {
-    public function testIndex()
-    {
+    use JwtTrait;
+
+    private $token;
+
+    private function createToken($role){
+        return $this->createJwtToken([
+            "id" => 1,
+            "role" => $role,
+            "email" => "john@doe.com"
+        ]);
+    }
+
+    public function testAdminCanAccessAllUsers(){
+        $token = $this->createToken('ROLE_ADMIN');
+        $headers = [
+            'HTTP_AUTHORIZATION' => "Bearer {$token}",
+            'CONTENT_TYPE' => 'application/json',
+        ];
         $client = static::createClient();
+        $crawler = $client->request('GET', '/users', [], [], $headers);
+        $this->assertEquals($client->getResponse()->getStatusCode(), 200);
+        $data = json_decode($client->getResponse()->getContent(), true);
+        $this->assertArrayHasKey('data', $data);
+    }
 
-        $crawler = $client->request('GET', '/');
+    public function testAdminCanAccessSpecificUserData(){
+        $token = $this->createToken('ROLE_ADMIN');
+        $headers = [
+            'HTTP_AUTHORIZATION' => "Bearer {$token}",
+            'CONTENT_TYPE' => 'application/json',
+        ];
+        $client = static::createClient();
+        $crawler = $client->request('GET', '/users/1', [],  [], $headers);
+        $this->assertEquals($client->getResponse()->getStatusCode(), 200);
+        $data = json_decode($client->getResponse()->getContent(), true);
+        $this->assertArrayHasKey('name', $data["data"]);
+    }
 
-        $this->assertContains('Hello World', $client->getResponse()->getContent());
+    public function testUserCanAccessOwnData(){
+        $token = $this->createToken('ROLE_USER');
+        $headers = [
+            'HTTP_AUTHORIZATION' => "Bearer {$token}",
+            'CONTENT_TYPE' => 'application/json',
+        ];
+        $client = static::createClient();
+        $crawler = $client->request('GET', '/users/1', [],  [], $headers);
+        $this->assertEquals($client->getResponse()->getStatusCode(), 200);
+        $data = json_decode($client->getResponse()->getContent(), true);
+        $this->assertArrayHasKey('name', $data["data"]);
+    }
+
+    public function testUserCannotAccessOtherUserData(){
+        $token = $this->createToken('ROLE_USER');
+        $headers = [
+            'HTTP_AUTHORIZATION' => "Bearer {$token}",
+            'CONTENT_TYPE' => 'application/json',
+        ];
+        $client = static::createClient();
+        $crawler = $client->request('POST', '/auth/register', [
+            'name' => 'john doe',
+            'email' => 'johndoe1@gmail.com',
+            'password' => 'password'
+        ]);
+        $client2 = static::createClient();
+        $crawler = $client2->request('GET', '/users/2', [],  [],  $headers);
+        $this->assertEquals($client2->getResponse()->getStatusCode(), 401);
     }
 }
